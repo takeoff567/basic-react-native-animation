4708b8548f73e846c5f96dff4da8268743a74f77
takeoff567 <puneet.web@ymtsindia.org>
2025-07-10 09:51:03 +0530

    learn interpolate

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import {
   Animated,
   Dimensions,
+  Easing,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -13,7 +14,30 @@ const DRAWER_WIDTH = 200;
 export default function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const translateX = useRef(new Animated.Value(-DRAWER_WIDTH)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: -200 })).current;
 
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const loop = Animated.loop(
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 3000,
+        easing: Easing.inOut(Easing.linear), // continuous sine-like easing
+        useNativeDriver: true,
+      }),
+    );
+
+    loop.start();
+
+    return () => loop.stop();
+  }, [animatedValue]);
+
+  // Interpolate Y movement as a sine wave motion between -200 to 200
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 0.25, 0.5, 0.75, 1],
+    outputRange: [0, -200, 0, 200, 0], // like bouncing back and forth
+  });
   const toggleDrawer = () => {
     Animated.timing(translateX, {
       toValue: isDrawerOpen ? -DRAWER_WIDTH : 0,
@@ -25,7 +49,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Main Screen</Text>
-
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            transform: [
+              { translateY }, // 👈 hourglass effect!
+            ],
+          },
+        ]}
+      >
+        <Text style={styles.text}>I'm sliding in!</Text>
+      </Animated.View>
       {/* Open Drawer Button */}
       {!isDrawerOpen && (
         <TouchableOpacity style={styles.openBtn} onPress={toggleDrawer}>
@@ -39,6 +74,7 @@ export default function App() {
           styles.drawer,
           {
             transform: [{ translateX }],
+            // scaleX,
           },
         ]}
       >
@@ -48,7 +84,7 @@ export default function App() {
         </TouchableOpacity>
 
         {/* Drawer Content */}
-        <Text style={styles.drawerText}>I'm a drawer!</Text>
+        <Animated.Text style={styles.drawerText}>I'm a drawer!</Animated.Text>
       </Animated.View>
     </View>
   );
@@ -65,6 +101,15 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     color: 'black',
+  },
+  box: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#4ade80',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 100,
   },
   drawer: {
     position: 'absolute',
